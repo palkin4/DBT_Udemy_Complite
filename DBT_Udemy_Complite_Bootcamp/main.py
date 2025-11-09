3e162e8fe9f0fb05cac055f7decd02a8c17149f0
Slavik <palkin4@gmail.com>
2025-11-09 17:34:40 -0500

    main.py creattion

--- DBT_Udemy_Complite_Bootcamp/main.py
+++ DBT_Udemy_Complite_Bootcamp/main.py
@@ -1,1 +1,10 @@
-print("helow world! ")
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+def main():
+    print("This is the main execution block.")
+    greet("Slava")
+
+if __name__ == "__main__":
+    main()
