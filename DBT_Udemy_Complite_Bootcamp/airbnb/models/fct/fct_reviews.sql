c8ec1d70b58d7c6105e8678339d2b3b485672330
Slavik <palkin4@gmail.com>
2025-11-14 09:47:59 -0500

    airbnb source and test module up to lesson 45

--- DBT_Udemy_Complite_Bootcamp/airbnb/models/fct/fct_reviews.sql
+++ DBT_Udemy_Complite_Bootcamp/airbnb/models/fct/fct_reviews.sql
@@ -11,5 +11,5 @@ select * from {{ref('src_reviews')}}
 where review_text is not null 
 
 {% if  is_incremental() %}
-    and  review_date >(select max(review_date) from {{this}})
+    and  review_date >(select max(review_date) from {{ this }})
 {% endif %}
--- DBT_Udemy_Complite_Bootcamp/airbnb/models/mart/mart_fullmoon_review.sql
+++ DBT_Udemy_Complite_Bootcamp/airbnb/models/mart/mart_fullmoon_review.sql
@@ -15,6 +15,6 @@ fill_moon_dates as(
 
 select 
 r.*
-,case when fm.full_moon_date is null then 'not full moon' else 'full moon ' end as is_full_moon
+,case when fm.full_moon_date is null then 'not full moon' else 'full moon' end as is_full_moon
 from fct_reviews r 
 left join fill_moon_dates fm on to_date(r.review_date)=dateadd(day,1,fm.full_moon_date)
