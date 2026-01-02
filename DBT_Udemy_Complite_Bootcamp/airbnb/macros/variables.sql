2048e8106dd58ad561a41f70c7aea2581537422f
Slavik <palkin4@gmail.com>
2026-01-02 11:46:02 -0500

    dbt fusion change

--- DBT_Udemy_Complite_Bootcamp/airbnb/macros/variables.sql
+++ DBT_Udemy_Complite_Bootcamp/airbnb/macros/variables.sql
@@ -1,7 +1,7 @@
 {% macro learn_variables() %}
 
     {% set your_name_jinja ="slavik" %}
-    {{log("Hello " ~ your_name_jinja ~ "!", info=True)}}
+    {{log("Hello palkin " ~ your_name_jinja ~ "!", info=True)}}
 
     {{log("hellow dbt user " ~ var("user_name") ~"!", info=True)}}
 {% endmacro %}
